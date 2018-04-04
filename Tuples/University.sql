db059dfd6cf0c62bae6f4b8809f169a1265f0b88
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-04-03 21:49:52 -0400

    Update University.sql

--- Tuples/University.sql
+++ Tuples/University.sql
@@ -1,6 +1,6 @@
 CREATE TABLE University(code CHAR(6) NOT NULL, name VARCHAR(30) , year_opened VARCHAR(30) , address VARCHAR(30), population INT, tuition INT,PRIMARY KEY(code));
 INSERT INTO University(code,name,year_opened,address,population,tuition) VALUES 
-('002629','Rutgers',1766,'NJ',68942,32191),
+('002629','Rutgers University',1766,'NJ',68942,32191),
 ('104659','Middlesex County College',1976,'NJ',2800,22071),
 ('458216','Pennsylvania State University',1855,'PA',99133,31556),
 ('789123','Rowan University',1923,'NJ',18484,25658),
