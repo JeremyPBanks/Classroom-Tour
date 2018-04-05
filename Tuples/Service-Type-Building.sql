48c6c53e8c5923225009726277ba0a90a0e19b8b
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-04-05 12:58:12 -0400

    Add files via upload

--- Tuples/Service-Type-Building.sql
+++ Tuples/Service-Type-Building.sql
@@ -1,2 +1,13 @@
 CREATE TABLE Service-Type-Building(campus_name VARCHAR(30) NOT NULL, code CHAR(6) NOT NULL , ID INT NOT NULL, roomID INT NOT NULL, floor_num INT, type VARCHAR(30), kind VARCHAR(30), product_focus VARCHAR(30),  PRIMARY KEY(campus_name,code,ID,roomID))
-INSERT INTO Service-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind, product_focus) VALUES
+INSERT INTO Service-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind,room_use VARCHAR(30), product_focus) VALUES
+
+('Main','789123',2,101,1,'Help and Information','Student Center','Student Lounge',),
+('Main','789123',2,102,1,'Help and Information','Student Center','Office',),
+('Main','789123',2,103,1,'Help and Information','Student Center','Help Desk',),
+('Main','789123',2,104,1,'Help and Information','Student Center','Study Room',),
+('Main','789123',2,105,1,'Help and Information','Student Center','Student Lounge',),
+('Main','789123',2,201,1,'Help and Information','Student Center','Student Lounge',),
+('Main','789123',2,202,1,'Help and Information','Student Center','Computer Lab',),
+('Main','789123',2,203,1,'Help and Information','Student Center','Study Room',),
+('Main','789123',2,204,1,'Help and Information','Student Center','Study Room',),
+('Main','789123',2,205,1,'Help and Information','Student Center','Study Room',),
