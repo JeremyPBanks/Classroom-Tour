8252a4dc06c29529732cdb215799df72e86aee91
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-04-05 15:45:26 -0400

    Add files via upload

--- Tuples/Service-Type-Building.sql
+++ Tuples/Service-Type-Building.sql
@@ -1,6 +1,22 @@
 CREATE TABLE Service-Type-Building(campus_name VARCHAR(30) NOT NULL, code CHAR(6) NOT NULL , ID INT NOT NULL, roomID INT NOT NULL, floor_num INT, type VARCHAR(30), kind VARCHAR(30), product_focus VARCHAR(30),  PRIMARY KEY(campus_name,code,ID,roomID))
 INSERT INTO Service-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind,room_use, product_focus) VALUES
 
+('Main','789123',20,100,1,'Help and Information','Admin Office','Mail Room',),
+('Main','789123',20,101,1,'Help and Information','Admin Office','Help Desk',),
+('Main','789123',20,102,1,'Help and Information','Admin Office','Office',),
+('Main','789123',20,103,1,'Help and Information','Admin Office','Office',),
+('Main','789123',20,104,1,'Help and Information','Admin Office','Office',),
+('Main','789123',20,105,1,'Help and Information','Admin Office','Office',),
+('Main','789123',20,106,1,'Help and Information','Admin Office','Office',),
+('Main','789123',20,107,1,'Help and Information','Admin Office','Office',),
+('Main','789123',20,108,1,'Help and Information','Admin Office','Office',),
+('Main','789123',20,109,1,'Help and Information','Admin Office','Office',),
+('Main','789123',17,100,1,'Food Service','Restaurant','Mail Room',),
+('Main','789123',17,101,1,'Food Service','Restaurant','Main Hall',),
+('Main','789123',17,102,1,'Food Service','Restaurant','Kitchen',),
+('Main','789123',16,100,1,'Food Service','Restaurant','Mail Room',),
+('Main','789123',16,101,1,'Food Service','Restaurant','Main Hall',),
+('Main','789123',16,102,1,'Food Service','Restaurant','Kitchen',),
 ('Main','789123',11,100,1,'General Store',,'Mail Room','Books'),
 ('Main','789123',11,101,1,'General Store',,'Fiction Section','Books'),
 ('Main','789123',11,102,1,'General Store',,'Non-Fiction Section','Books'),
@@ -11,12 +27,10 @@ INSERT INTO Service-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind
 ('Main','789123',11,203,2,'General Store',,'Study Room','Books'),
 ('Main','789123',11,204,2,'General Store',,'Study Room','Books'),
 ('Main','789123',11,205,2,'General Store',,'Study Room','Books'),
-
 ('Main','789123',10,100,1,'Food Service','Dining Hall','Mail Room',),
 ('Main','789123',10,101,1,'Food Service','Dining Hall','Main Hall',),
 ('Main','789123',10,102,1,'Food Service','Dining Hall','Kitchen',),
 ('Main','789123',10,103,1,'Food Service','Dining Hall','Faculty Hall',),
-
 ('Main','789123',6,100,1,'Help and Information','Library','Mail Room',),
 ('Main','789123',6,101,1,'Help and Information','Library','Main Hall',),
 ('Main','789123',6,102,1,'Help and Information','Library','Office',),
@@ -49,7 +63,6 @@ INSERT INTO Service-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind
 ('Main','789123',6,408,4,'Help and Information','Library','Study Room',),
 ('Main','789123',6,409,4,'Help and Information','Library','Study Room',),
 ('Main','789123',6,410,4,'Help and Information','Library','Study Room',),
-
 ('Main','789123',2,100,1,'Help and Information','Student Center','Mail Room',),
 ('Main','789123',2,101,1,'Help and Information','Student Center','Student Lounge',),
 ('Main','789123',2,102,1,'Help and Information','Student Center','Office',),
@@ -61,7 +74,6 @@ INSERT INTO Service-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind
 ('Main','789123',2,203,2,'Help and Information','Student Center','Study Room',),
 ('Main','789123',2,204,2,'Help and Information','Student Center','Study Room',),
 ('Main','789123',2,205,2,'Help and Information','Student Center','Study Room',),
-
 ('Main','789123',9,100,1,'Help and Information','IT','Mail Room',),
 ('Main','789123',9,101,1,'Help and Information','IT','Office',),
 ('Main','789123',9,102,1,'Help and Information','IT','Office',),
