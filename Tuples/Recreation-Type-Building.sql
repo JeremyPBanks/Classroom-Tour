907119c3409181d1b72990946a9e8b5db6f4b03e
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-04-05 14:24:44 -0400

    Add files via upload

--- Tuples/Recreation-Type-Building.sql
+++ Tuples/Recreation-Type-Building.sql
@@ -1,2 +1,20 @@
-CREATE TABLE Recreation-Type-Building(campus_name VARCHAR(30) NOT NULL, code CHAR(6) NOT NULL , ID INT NOT NULL, roomID INT NOT NULL, floor_num INT, type VARCHAR(30), kind VARCHAR(30), has_gym VARCHAR(30), has_sauna VARCHAR(30), has_pool VARCHAR(30), has_weight_room VARCHAR(30), PRIMARY KEY(campus_name,code,ID,roomID))
-INSERT INTO Recreation-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind,has_gym,has_sauna,has_pool,has_weight_room) VALUES
+CREATE TABLE Recreation-Type-Building(campus_name VARCHAR(30) NOT NULL, code CHAR(6) NOT NULL , ID INT NOT NULL, roomID INT NOT NULL, floor_num INT, type VARCHAR(30), kind VARCHAR(30), room_use VARCHAR(30), PRIMARY KEY(campus_name,code,ID,roomID))
+INSERT INTO Recreation-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind,room_use) VALUES
+
+('Main','789123',7,100,1,'Health & Lifestyle','Gym','Mail Room'),
+('Main','789123',7,101,1,'Health & Lifestyle','Gym','Office'),
+('Main','789123',7,102,1,'Health & Lifestyle','Gym','Office'),
+('Main','789123',7,103,1,'Health & Lifestyle','Gym','Fitness Room'),
+('Main','789123',7,104,1,'Health & Lifestyle','Gym','Weight Room'),
+('Main','789123',7,105,1,'Health & Lifestyle','Gym','Weight Room'),
+('Main','789123',7,106,1,'Health & Lifestyle','Gym','Basketball Court'),
+('Main','789123',7,107,1,'Health & Lifestyle','Gym','Basketball Court'),
+('Main','789123',7,108,1,'Health & Lifestyle','Gym','Rock-Climbing Room'),
+('Main','789123',7,109,1,'Health & Lifestyle','Gym','Racquetball Court'),
+('Main','789123',7,110,1,'Health & Lifestyle','Gym','Swimming Pool'),
+('Main','789123',7,111,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
+('Main','789123',7,112,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
+('Main','789123',7,113,1,'Health & Lifestyle','Gym','Men Locker Room'),
+('Main','789123',7,114,1,'Health & Lifestyle','Gym','Women Locker Room'),
+('Main','789123',7,115,1,'Health & Lifestyle','Gym','Neutral Locker Room'),
+
