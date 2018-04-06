c406f11c245f1cf6eea381fb25591737c5b29fca
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-04-06 13:42:10 -0400

    Update Service-Type-Building.sql

--- Tuples/Yale/Service-Type-Building.sql
+++ Tuples/Yale/Service-Type-Building.sql
@@ -1,4 +1,3 @@
-CREATE TABLE Service-Type-Building(campus_name VARCHAR(30) NOT NULL, code CHAR(6) NOT NULL , ID INT NOT NULL, roomID INT NOT NULL, floor_num INT, type VARCHAR(30), kind VARCHAR(30), product_focus VARCHAR(30),  PRIMARY KEY(campus_name,code,ID,roomID))
 INSERT INTO Service-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind,room_use, product_focus) VALUES
 
 ('Main','924056',20,100,1,'Help and Information','Admin Office','Mail Room',NULL),
