d8d7ebe01f8d0e6d338bf6e1ffde027590f546a8
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-04-06 08:38:01 -0400

    Update Recreation-Type-Building.sql

--- Tuples/MSCC/Recreation-Type-Building.sql
+++ Tuples/MSCC/Recreation-Type-Building.sql
@@ -15,39 +15,3 @@ INSERT INTO Recreation-Type-Building(campus_name,code,ID,roomID,floor_num,type,k
 ('Main','104659',5,110,1,'Health & Lifestyle','Gym','Neutral Locker Room'),
 ('Main','104659',5,111,1,'Health & Lifestyle','Gym','Office'),
 ('Main','104659',5,112,1,'Health & Lifestyle','Gym','Office'),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-('Main','789123',7,100,1,'Health & Lifestyle','Gym','Mail Room'),
-('Main','789123',7,101,1,'Health & Lifestyle','Gym','Office'),
-('Main','789123',7,102,1,'Health & Lifestyle','Gym','Office'),
-('Main','789123',7,103,1,'Health & Lifestyle','Gym','Fitness Room'),
-('Main','789123',7,104,1,'Health & Lifestyle','Gym','Weight Room'),
-('Main','789123',7,105,1,'Health & Lifestyle','Gym','Weight Room'),
-('Main','789123',7,106,1,'Health & Lifestyle','Gym','Basketball Court'),
-('Main','789123',7,107,1,'Health & Lifestyle','Gym','Basketball Court'),
-('Main','789123',7,108,1,'Health & Lifestyle','Gym','Rock-Climbing Room'),
-('Main','789123',7,109,1,'Health & Lifestyle','Gym','Racquetball Court'),
-('Main','789123',7,110,1,'Health & Lifestyle','Gym','Swimming Pool'),
-('Main','789123',7,111,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
-('Main','789123',7,112,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
-('Main','789123',7,113,1,'Health & Lifestyle','Gym','Men Locker Room'),
-('Main','789123',7,114,1,'Health & Lifestyle','Gym','Women Locker Room'),
-('Main','789123',7,115,1,'Health & Lifestyle','Gym','Neutral Locker Room'),
-
