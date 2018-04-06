5b54a34626001f306d8452061ef47504630b855e
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-04-06 13:37:04 -0400

    Add files via upload

---
+++ Tuples/Recreation-Type-Building.sql
@@ -0,0 +1,86 @@
+CREATE TABLE Recreation-Type-Building(campus_name VARCHAR(30) NOT NULL, code CHAR(6) NOT NULL , ID INT NOT NULL, roomID INT NOT NULL, floor_num INT, type VARCHAR(30), kind VARCHAR(30), room_use VARCHAR(30), PRIMARY KEY(campus_name,code,ID,roomID))
+INSERT INTO Recreation-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind,room_use) VALUES
+
+
+
+('Main','924056',7,100,1,'Health & Lifestyle','Gym','Mail Room'),
+('Main','924056',7,101,1,'Health & Lifestyle','Gym','Office'),
+('Main','924056',7,102,1,'Health & Lifestyle','Gym','Fitness Room'),
+('Main','924056',7,103,1,'Health & Lifestyle','Gym','Weight Room'),
+('Main','924056',7,104,1,'Health & Lifestyle','Gym','Basketball Court'),
+('Main','924056',7,105,1,'Health & Lifestyle','Gym','Swimming Pool'),
+('Main','924056',7,106,1,'Health & Lifestyle','Gym','Swimming Pool'),
+('Main','924056',7,107,1,'Health & Lifestyle','Gym','Swimming Pool'),
+('Main','924056',7,108,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
+('Main','924056',7,109,1,'Health & Lifestyle','Gym','Men Locker Room'),
+('Main','924056',7,110,1,'Health & Lifestyle','Gym','Women Locker Room'),
+('Main','924056',7,111,1,'Health & Lifestyle','Gym','Neutral Locker Room'),
+('Main','924056',7,112,1,'Health & Lifestyle','Gym','Office'),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+('Main','104659',5,100,1,'Health & Lifestyle','Gym','Mail Room'),
+('Main','104659',5,101,1,'Health & Lifestyle','Gym','Office'),
+('Main','104659',5,102,1,'Health & Lifestyle','Gym','Office'),
+('Main','104659',5,103,1,'Health & Lifestyle','Gym','Fitness Room'),
+('Main','104659',5,104,1,'Health & Lifestyle','Gym','Weight Room'),
+('Main','104659',5,105,1,'Health & Lifestyle','Gym','Basketball Court'),
+('Main','104659',5,106,1,'Health & Lifestyle','Gym','Swimming Pool'),
+('Main','104659',5,107,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
+('Main','104659',5,108,1,'Health & Lifestyle','Gym','Men Locker Room'),
+('Main','104659',5,109,1,'Health & Lifestyle','Gym','Women Locker Room'),
+('Main','104659',5,110,1,'Health & Lifestyle','Gym','Neutral Locker Room'),
+('Main','104659',5,111,1,'Health & Lifestyle','Gym','Office'),
+('Main','104659',5,112,1,'Health & Lifestyle','Gym','Office'),
+
+
+
+('Main','789123',7,100,1,'Health & Lifestyle','Gym','Mail Room'),
+('Main','789123',7,101,1,'Health & Lifestyle','Gym','Office'),
+('Main','789123',7,102,1,'Health & Lifestyle','Gym','Office'),
+('Main','789123',7,103,1,'Health & Lifestyle','Gym','Fitness Room'),
+('Main','789123',7,104,1,'Health & Lifestyle','Gym','Weight Room'),
+('Main','789123',7,105,1,'Health & Lifestyle','Gym','Weight Room'),
+('Main','789123',7,106,1,'Health & Lifestyle','Gym','Basketball Court'),
+('Main','789123',7,107,1,'Health & Lifestyle','Gym','Basketball Court'),
+('Main','789123',7,108,1,'Health & Lifestyle','Gym','Rock-Climbing Room'),
+('Main','789123',7,109,1,'Health & Lifestyle','Gym','Racquetball Court'),
+('Main','789123',7,110,1,'Health & Lifestyle','Gym','Swimming Pool'),
+('Main','789123',7,111,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
+('Main','789123',7,112,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
+('Main','789123',7,113,1,'Health & Lifestyle','Gym','Men Locker Room'),
+('Main','789123',7,114,1,'Health & Lifestyle','Gym','Women Locker Room'),
+('Main','789123',7,115,1,'Health & Lifestyle','Gym','Neutral Locker Room'),
+
---
+++ Tuples/Service-Type-Building.sql
@@ -0,0 +1,312 @@
+CREATE TABLE Service-Type-Building(campus_name VARCHAR(30) NOT NULL, code CHAR(6) NOT NULL , ID INT NOT NULL, roomID INT NOT NULL, floor_num INT, type VARCHAR(30), kind VARCHAR(30), product_focus VARCHAR(30),  PRIMARY KEY(campus_name,code,ID,roomID))
+INSERT INTO Service-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind,room_use, product_focus) VALUES
+
+('Main','924056',20,100,1,'Help and Information','Admin Office','Mail Room',NULL),
+('Main','924056',20,101,1,'Help and Information','Admin Office','Help Desk',NULL),
+('Main','924056',20,102,1,'Help and Information','Admin Office','Office',NULL),
+('Main','924056',20,103,1,'Help and Information','Admin Office','Office',NULL),
+('Main','924056',20,104,1,'Help and Information','Admin Office','Office',NULL),
+('Main','924056',20,105,1,'Help and Information','Admin Office','Office',NULL),
+('Main','924056',20,106,1,'Help and Information','Admin Office','Office',NULL),
+
+('Main','924056',17,100,1,'Food Service','Restaurant','Mail Room',NULL),
+('Main','924056',17,101,1,'Food Service','Restaurant','Main Hall',NULL),
+('Main','924056',17,102,1,'Food Service','Restaurant','Kitchen',NULL),
+
+('Main','924056',16,100,1,'Food Service','Restaurant','Mail Room',NULL),
+('Main','924056',16,101,1,'Food Service','Restaurant','Main Hall',NULL),
+('Main','924056',16,102,1,'Food Service','Restaurant','Kitchen',NULL),
+
+('Main','924056',12,100,1,'General Store',NULL,'Mail Room','General School Supplies'),
+('Main','924056',12,101,1,'General Store',NULL,'Store Floor','General School Supplies'),
+('Main','924056',12,201,1,'General Store',NULL,'Store Floor','General School Supplies'),
+
+('Main','924056',11,100,1,'General Store',NULL,'Mail Room','General School Supplies'),
+('Main','924056',11,101,1,'General Store',NULL,'Store Floor','General School Supplies'),
+('Main','924056',11,201,1,'General Store',NULL,'Store Floor','General School Supplies'),
+
+('Main','924056',10,100,1,'Food Service','Dining Hall','Mail Room',NULL),
+('Main','924056',10,101,1,'Food Service','Dining Hall','Main Hall',NULL),
+('Main','924056',10,102,1,'Food Service','Dining Hall','Kitchen',NULL),
+('Main','924056',10,103,1,'Food Service','Dining Hall','Faculty Hall',NULL),
+('Main','924056',10,201,2,'Food Service','Dining Hall','Main Hall',NULL),
+('Main','924056',10,202,2,'Food Service','Dining Hall','Faculty Hall',NULL),
+
+('Main','924056',9,100,1,'Help and Information','IT','Mail Room',NULL),
+('Main','924056',9,101,1,'Help and Information','IT','Office',NULL),
+('Main','924056',9,102,1,'Help and Information','IT','Office',NULL),
+('Main','924056',9,103,1,'Help and Information','IT','Help Desk',NULL),
+('Main','924056',9,104,1,'Help and Information','IT','Help Desk',NULL),
+('Main','924056',9,105,1,'Help and Information','IT','Help Desk',NULL),
+('Main','924056',9,106,1,'Help and Information','IT','Office',NULL),
+('Main','924056',9,107,1,'Help and Information','IT','Office',NULL),
+('Main','924056',9,108,1,'Help and Information','IT','Office',NULL),
+('Main','924056',9,109,1,'Help and Information','IT','Office',NULL),
+
+('Main','924056',6,100,1,'Help and Information','Library','Mail Room',NULL),
+('Main','924056',6,101,1,'Help and Information','Library','Main Hall',NULL),
+('Main','924056',6,102,1,'Help and Information','Library','Office',NULL),
+('Main','924056',6,103,1,'Help and Information','Library','Help Desk',NULL),
+('Main','924056',6,104,1,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,105,1,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,106,1,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,107,1,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,108,1,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,109,1,'Help and Information','Library','Student Lounge',NULL),
+('Main','924056',6,201,2,'Help and Information','Library','Main Hall',NULL),
+('Main','924056',6,202,2,'Help and Information','Library','Student Lounge',NULL),
+('Main','924056',6,203,2,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,204,2,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,205,2,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,206,2,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,207,2,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,208,2,'Help and Information','Library','Office',NULL),
+('Main','924056',6,209,2,'Help and Information','Library','Office',NULL),
+('Main','924056',6,210,2,'Help and Information','Library','Office',NULL),
+('Main','924056',6,301,3,'Help and Information','Library','Main Hall',NULL),
+('Main','924056',6,302,3,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,303,3,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,304,3,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,305,3,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,306,3,'Help and Information','Library','Student Lounge',NULL),
+('Main','924056',6,307,3,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,308,3,'Help and Information','Library','Study Room',NULL),
+('Main','924056',6,309,3,'Help and Information','Library','Office',NULL),
+('Main','924056',6,310,3,'Help and Information','Library','Office',NULL),
+('Main','924056',6,311,3,'Help and Information','Library','Office',NULL),
+
+
+('Main','924056',2,100,1,'Help and Information','Student Center','Mail Room',NULL),
+('Main','924056',2,101,1,'Help and Information','Student Center','Computer Lab',NULL),
+('Main','924056',2,102,1,'Help and Information','Student Center','Help Desk',NULL),
+('Main','924056',2,103,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','924056',2,104,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','924056',2,105,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','924056',2,106,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','924056',2,107,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','924056',2,108,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','924056',2,109,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','924056',2,110,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','924056',2,111,1,'Help and Information','Student Center','Student Lounge',NULL),
+('Main','924056',2,112,1,'Help and Information','Student Center','Office',NULL),
+('Main','924056',2,113,1,'Help and Information','Student Center','Office',NULL),
+('Main','924056',2,114,1,'Help and Information','Student Center','Office',NULL),
+('Main','924056',2,115,1,'Help and Information','Student Center','Office',NULL),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+('Main','789123',13,100,1,'Food Service','Restaurant','Mail Room',NULL),
+('Main','789123',13,101,1,'Food Service','Restaurant','Main Hall',NULL),
+('Main','789123',13,102,1,'Food Service','Restaurant','Kitchen',NULL),
+
+('Main','104659',9,100,1,'General Store',NULL,'Mail Room','General School Supplies'),
+('Main','104659',9,101,1,'General Store',NULL,'Store Floor','General School Supplies'),
+
+('Main','104659',8,100,1,'Food Service','Dining Hall','Mail Room',NULL),
+('Main','104659',8,101,1,'Food Service','Dining Hall','Main Hall',NULL),
+('Main','104659',8,102,1,'Food Service','Dining Hall','Kitchen',NULL),
+('Main','104659',8,103,1,'Food Service','Dining Hall','Faculty Hall',NULL),
+
+('Main','104659',4,100,1,'Help and Information','Library','Mail Room',NULL),
+('Main','104659',4,101,1,'Help and Information','Library','Main Hall',NULL),
+('Main','104659',4,102,1,'Help and Information','Library','Office',NULL),
+('Main','104659',4,103,1,'Help and Information','Library','Help Desk',NULL),
+('Main','104659',4,104,1,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,105,1,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,106,1,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,107,1,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,108,1,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,109,1,'Help and Information','Library','Student Lounge',NULL),
+('Main','104659',4,201,2,'Help and Information','Library','Main Hall',NULL),
+('Main','104659',4,202,2,'Help and Information','Library','Student Lounge',NULL),
+('Main','104659',4,203,2,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,204,2,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,205,2,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,301,3,'Help and Information','Library','Main Hall',NULL),
+('Main','104659',4,302,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,303,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,304,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,305,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,306,3,'Help and Information','Library','Student Lounge',NULL),
+('Main','104659',4,307,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,308,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,309,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,310,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,311,3,'Help and Information','Library','Study Room',NULL),
+('Main','104659',4,401,4,'Help and Information','Library','Main Hall',NULL),
+('Main','104659',4,402,4,'Help and Information','Library','Help Desk',NULL),
+('Main','104659',4,403,4,'Help and Information','Library','Faculty Lounge',NULL),
+('Main','104659',4,404,4,'Help and Information','Library','Office',NULL),
+('Main','104659',4,405,4,'Help and Information','Library','Office',NULL),
+('Main','104659',4,406,4,'Help and Information','Library','Office',NULL),
+('Main','104659',4,407,4,'Help and Information','Library','Office',NULL),
+('Main','104659',4,408,4,'Help and Information','Library','Office',NULL),
+('Main','104659',4,409,4,'Help and Information','Library','Office',NULL),
+('Main','104659',4,410,4,'Help and Information','Library','Office',NULL),
+
+('Main','104659',2,100,1,'Help and Information','Student Center','Mail Room',NULL),
+('Main','104659',2,101,1,'Help and Information','Student Center','Student Lounge',NULL),
+('Main','104659',2,102,1,'Help and Information','Student Center','Office',NULL),
+('Main','104659',2,103,1,'Help and Information','Student Center','Help Desk',NULL),
+('Main','104659',2,104,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','104659',2,105,1,'Help and Information','Student Center','Student Lounge',NULL),
+('Main','104659',2,106,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','104659',2,107,1,'Help and Information','Student Center','Computer Lab',NULL),
+('Main','104659',2,108,1,'Help and Information','Student Center','Office',NULL),
+('Main','104659',2,109,1,'Help and Information','Student Center','Office',NULL),
+('Main','104659',2,110,1,'Help and Information','Student Center','Study Room',NULL),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+('Main','789123',20,100,1,'Help and Information','Admin Office','Mail Room',NULL),
+('Main','789123',20,101,1,'Help and Information','Admin Office','Help Desk',NULL),
+('Main','789123',20,102,1,'Help and Information','Admin Office','Office',NULL),
+('Main','789123',20,103,1,'Help and Information','Admin Office','Office',NULL),
+('Main','789123',20,104,1,'Help and Information','Admin Office','Office',NULL),
+('Main','789123',20,105,1,'Help and Information','Admin Office','Office',NULL),
+('Main','789123',20,106,1,'Help and Information','Admin Office','Office',NULL),
+('Main','789123',20,107,1,'Help and Information','Admin Office','Office',NULL),
+('Main','789123',20,108,1,'Help and Information','Admin Office','Office',NULL),
+('Main','789123',20,109,1,'Help and Information','Admin Office','Office',NULL),
+('Main','789123',17,100,1,'Food Service','Restaurant','Mail Room',NULL),
+('Main','789123',17,101,1,'Food Service','Restaurant','Main Hall',NULL),
+('Main','789123',17,102,1,'Food Service','Restaurant','Kitchen',NULL),
+('Main','789123',16,100,1,'Food Service','Restaurant','Mail Room',NULL),
+('Main','789123',16,101,1,'Food Service','Restaurant','Main Hall',NULL),
+('Main','789123',16,102,1,'Food Service','Restaurant','Kitchen',NULL),
+('Main','789123',11,100,1,'General Store',,'Mail Room','Books'),
+('Main','789123',11,101,1,'General Store',,'Fiction Section','Books'),
+('Main','789123',11,102,1,'General Store',,'Non-Fiction Section','Books'),
+('Main','789123',11,103,1,'General Store',,'Textbooks','Books'),
+('Main','789123',11,104,1,'Food Service','Coffee Shop',,'Coffee'),
+('Main','789123',11,201,2,'General Store',,'Study Room','Books'),
+('Main','789123',11,202,2,'General Store',,'Study Room','Books'),
+('Main','789123',11,203,2,'General Store',,'Study Room','Books'),
+('Main','789123',11,204,2,'General Store',,'Study Room','Books'),
+('Main','789123',11,205,2,'General Store',,'Study Room','Books'),
+('Main','789123',10,100,1,'Food Service','Dining Hall','Mail Room',NULL),
+('Main','789123',10,101,1,'Food Service','Dining Hall','Main Hall',NULL),
+('Main','789123',10,102,1,'Food Service','Dining Hall','Kitchen',NULL),
+('Main','789123',10,103,1,'Food Service','Dining Hall','Faculty Hall',NULL),
+('Main','789123',6,100,1,'Help and Information','Library','Mail Room',NULL),
+('Main','789123',6,101,1,'Help and Information','Library','Main Hall',NULL),
+('Main','789123',6,102,1,'Help and Information','Library','Office',NULL),
+('Main','789123',6,103,1,'Help and Information','Library','Help Desk',NULL),
+('Main','789123',6,104,1,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,105,1,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,201,2,'Help and Information','Library','Main Hall',NULL),
+('Main','789123',6,202,2,'Help and Information','Library','Student Lounge',NULL),
+('Main','789123',6,203,2,'Help and Information','Library','Student Lounge',NULL),
+('Main','789123',6,204,2,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,205,2,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,301,3,'Help and Information','Library','Main Hall',NULL),
+('Main','789123',6,302,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,303,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,304,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,305,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,306,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,307,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,308,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,309,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,310,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,311,3,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,401,4,'Help and Information','Library','Main Hall',NULL),
+('Main','789123',6,402,4,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,403,4,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,404,4,'Help and Information','Library','Student Lounge',NULL),
+('Main','789123',6,405,4,'Help and Information','Library','Office',NULL),
+('Main','789123',6,406,4,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,407,4,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,408,4,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,409,4,'Help and Information','Library','Study Room',NULL),
+('Main','789123',6,410,4,'Help and Information','Library','Study Room',NULL),
+('Main','789123',2,100,1,'Help and Information','Student Center','Mail Room',NULL),
+('Main','789123',2,101,1,'Help and Information','Student Center','Student Lounge',NULL),
+('Main','789123',2,102,1,'Help and Information','Student Center','Office',NULL),
+('Main','789123',2,103,1,'Help and Information','Student Center','Help Desk',NULL),
+('Main','789123',2,104,1,'Help and Information','Student Center','Study Room',NULL),
+('Main','789123',2,105,1,'Help and Information','Student Center','Student Lounge',NULL),
+('Main','789123',2,201,2,'Help and Information','Student Center','Student Lounge',NULL),
+('Main','789123',2,202,2,'Help and Information','Student Center','Computer Lab',NULL),
+('Main','789123',2,203,2,'Help and Information','Student Center','Study Room',NULL),
+('Main','789123',2,204,2,'Help and Information','Student Center','Study Room',NULL),
+('Main','789123',2,205,2,'Help and Information','Student Center','Study Room',NULL),
+('Main','789123',9,100,1,'Help and Information','IT','Mail Room',NULL),
+('Main','789123',9,101,1,'Help and Information','IT','Office',NULL),
+('Main','789123',9,102,1,'Help and Information','IT','Office',NULL),
+('Main','789123',9,103,1,'Help and Information','IT','Help Desk',NULL),
+('Main','789123',9,104,1,'Help and Information','IT','Help Desk',NULL),
+('Main','789123',9,105,1,'Help and Information','IT','Help Desk',NULL),
