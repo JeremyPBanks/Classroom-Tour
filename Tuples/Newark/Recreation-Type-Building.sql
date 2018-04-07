fa4aecc4ed7a89d79c787c6b2ca1eefdbd6af25c
Neel <send2neelpatel@gmail.com>
2018-04-06 23:39:48 -0400

    rec commit

---
+++ Tuples/Newark/Recreation-Type-Building.sql
@@ -0,0 +1,14 @@
+INSERT INTO Recreation-Type-Building(campus_name,code,ID,roomID,floor_num,type,kind,room_use) VALUES
+
+('Newark','002629',14,100,1,'Health & Lifestyle','Gym','Mail Room'),
+('Newark','002629',14,101,1,'Health & Lifestyle','Gym','Office'),
+('Newark','002629',14,102,1,'Health & Lifestyle','Gym','Office'),
+('Newark','002629',14,103,1,'Health & Lifestyle','Gym','Basketball Court'),
+('Newark','002629',14,104,1,'Health & Lifestyle','Gym','Basketball Court'),
+('Newark','002629',14,105,1,'Health & Lifestyle','Gym','Multi-Purpose Room'),
+('Newark','002629',14,106,1,'Health & Lifestyle','Gym','Men Locker Room'),
+('Newark','002629',14,107,1,'Health & Lifestyle','Gym','Women Locker Room'),
+('Newark','002629',14,108,1,'Health & Lifestyle','Gym','Fitness Room'),
+('Newark','002629',14,109,1,'Health & Lifestyle','Gym','Weight Room'),
+('Newark','002629',14,110,1,'Health & Lifestyle','Gym','Men Locker Room'),
+('Newark','002629',14,111,1,'Health & Lifestyle','Gym','Women Locker Room'),
